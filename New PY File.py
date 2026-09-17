b3190ca0d50d8dc36bddab750325d7bb63e4ac76
jaipalreddy-3993 <jaipalreddy1590@gmail.com>
2026-09-17 15:54:50 +0530

    Add files via upload

---
+++ New PY File.py
@@ -0,0 +1,61 @@
+print("Hello World!")
+print("Welcome to Python")
+print("Jaipal")
+print("I am learning Python")
+print("Python Programming")
+print("Good morning")
+print("Welcome students")
+print("Python is easy")
+print("My first programme")
+print("Hello from Python")
+
+print("2+5")
+print(2+5)
+print("10-3")
+print(10-3)
+print("10+20")
+print(10+20)
+
+name = "Jaipal"
+age = 20
+marks = 85
+price = 99.5
+city = "Chennai"
+score = 95
+x = 10
+print(x)
+message = "Welcome"
+print(message)
+a = 10
+b = 20
+print(a+b)
+quantity = 5
+price = 10
+print(quantity*price)
+
+age = 21
+height = 5.8
+name = "Arun"
+age = 20
+price = 99.5
+name = "Ravi"
+
+print("Start, "), print("Middle, "), print("End")
+x = 10
+print(x)
+x = 20
+print(x)
+number = 5
+print(number)
+number = 10
+print(number)
+print("A, B, C")
+name = "Ravi"
+age = 20
+print(name, age)
+print("1, 2, 3, 4.")
+x = 5
+y = 10
+print(x, y)
+print("Start", 2+3, "End")
+print("Login, Dashboard, Logout.")
